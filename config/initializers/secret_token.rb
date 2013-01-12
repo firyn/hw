96b3c601ad384b3b0072301f2ee3c9d80e0e28ee
Firyn <firyn666@live.com>
2013-01-12 17:01:45 -0500

    Upload HW rails app for heroku test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw::Application.config.secret_token = 'e890d03691445f59f942cc370d4c7e96655fcc4e49ff4939803f732c780787e5d09cc4ad0108f7dbe3955453f6722ef5d4f10963d03bb5598508e0048cdbb1ed'
